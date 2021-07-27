000ba0539bc828440ada389783b46f6fd38b9229
Long Tô <67490989+ThanhLongTo@users.noreply.github.com>
2021-07-27 20:46:40 +0700

    Update data_scrape.py

--- data_scrape.py
+++ data_scrape.py
@@ -53,3 +53,72 @@ for i in range(RANGE_SEARCH):
     time.sleep(0.4)
     i += 1
     continue
+
+    
+    
+    
+    
+    
+# SCRAPE ĐIỂM THPT 2021
+
+
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+
+def fetch_data(sbd):
+    headers = {
+        'authority': 'diemthi.tuoitre.vn',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+        'referer': 'https://diemthi.tuoitre.vn/thpt-2021',
+
+    }
+    payload = {"data": str(sbd), "code": ""}
+
+    page = requests.post("https://diemthi.tuoitre.vn/search-thpt-score", headers=headers, data=payload)
+    print(page.status_code)
+
+    html = BeautifulSoup(page.content, "html.parser")
+
+    a = json.loads(str(html))
+
+    return a
+
+
+# Sample response form : data = {"data":[{"_id":"S9D33noBrZdVKVdyg4Fk","_index":"student_thpt","_score":0,"_source":{"examination_id":1,"name":"","sbd":"01000020","school_id":49,"score":"Toán:8.80;Văn:6.25;Lí:5.50;Hóa:8.25;Sinh:4.50;Sử:;Địa:;GDCD:;Ngoại Ngữ:8.40;"},"_type":"_doc"}],"message":"Suscess!","status":200}
+
+def analyse(data):
+    # pretty = json.dumps(data, indent=4,ensure_ascii=False)
+
+    sbd = data["data"][0]["_source"]["sbd"]
+    score_list = data["data"][0]["_source"]["score"].split(";")
+
+    score_extract = {}
+    for i in score_list:
+        score_extract[i[:i.find(":")]] = i[i.find(":")+1:]
+
+    return [sbd,score_extract]
+
+if __name__ == "__main__" :
+
+    RANGE_SEARCH = 100
+    main_data = {}
+    sbd_error = []
+
+    for i in range(1,RANGE_SEARCH):
+        sbd = "0" + str(1000000+i)
+
+        try:
+            data = fetch_data(sbd=sbd)
+            result = analyse(data)
+            print("pass")
+            main_data[str(result[0])] = result[1]
+            # print(result)
+            time.sleep(0.2)
+        except Exception:
+            print(sbd + " error")
+            sbd_error.append(sbd)
+    
+    print(main_data)
+    print(sbd_error)
